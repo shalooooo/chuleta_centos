584cef7e4e6be99f95c388066a973f42eafdce55
shalooooo <gonzalopvargas@gmail.com>
2020-02-22 14:41:24 -0300

    Update chuleta_centos.py

--- chuleta_centos.py
+++ chuleta_centos.py
@@ -265,7 +265,25 @@ dpkg-query -f '${binary:Package}\n' -W
 # Buscar en el cache de apt programas que calcen con python3-pip
 apt-cache search python3-pip
 
-
-
-
+#######################################################################################################
+init
+#######################################################################################################
+The best solution to know about these init levels is to understand the ” man init ” command output on Unix.
+There are basically 8 runlevels in unix. I will briefly tell some thing about the different init levels and their use.
+Run Level:  At any given time, the system is in one  of  eight  possible run  levels.  A  run level is a software configuration under which only a selected group of processes  exists.  Processes spawned  by init for each of these run levels are defined in /etc/inittab. init can be in one of eight  run  levels,  0-6 and  S  or  s (S and s are identical). The run level changes when a privileged user runs /sbin/init.
+init 0 :  Shutdown (goes thru the /etc/rc0.d/* scripts then halts)
+init 1  :  Single user mode or emergency mode means no network no multitasking is present in this mode only root has access in this runlevel
+init 2  :  No network but multitasking support is present .
+init 3  :  Network is present multitasking is present but with out GUI .
+init 4  :  It is similar to runlevel 3; It is reserved for other purposes in research.
+init 5  :  Network is present multitasking and GUI is present with sound etc.
+init 6  :  This runlevel is defined to system restart.
+init s   : Tells the init command to enter the maintenance mode. When the
+system enters maintenance mode from another run level, only the system console
+is used as the terminal.
+init S  : Same as init s.
+init m : Same as init s and init S.
+init M : Same as init s or init S or init m.
+We can take it from above that 4 options(S,s,M,m) are synonymous.
+#######################################################################################################
 
